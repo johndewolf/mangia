d40ad8530dd5efd3aa7064465ba2573775e657e0
John DeWolf <john.dewolf@pantheon.io>
2023-01-09 09:57:53 -0500

    migrate from sqlite to supabase and postgresql

--- prisma/migrations/20220706164721_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[slug]` on the table `Recipe` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Recipe_slug_key" ON "Recipe"("slug");
